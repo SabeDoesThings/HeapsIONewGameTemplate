e52b6463294f39aee82b088b58f70ab1f43f1ecd
Sabe <122580233+Sup0088@users.noreply.github.com>
2023-09-07 18:48:12 -0500

    Add files via upload

---
+++ src/Game.hx
@@ -0,0 +1,18 @@
+import hxd.Res;
+import hxd.App;
+
+class Game extends App {
+    override function init() {
+        var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
+        tf.text = "Hello Heaps.io";
+    }
+
+    override function update(dt: Float) {
+        
+    }
+
+    static function main() {
+        new Game();
+		Res.initLocal();
+    }
+}
