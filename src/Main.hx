53c9d30f82c7fffa7daf58c490c891c1e632d3f1
SabeDoesThings <122580233+SabeDoesThings@users.noreply.github.com>
2024-04-13 16:04:22 -0500

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -1,7 +1,7 @@
 class Main extends hxd.App {
     override function init() {
         var tf = new h2d.Text(hxd.res.DefaultFont.get(), s2d);
-        tf.text = "Hello Hashlink !";
+        tf.text = "Hello Heaps.io!";
         tf.scale(4);
     }
 
