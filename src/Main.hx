5ffd99cfffe66b820c1a2d47f7f407f014db0b8b
SabeDoesThings <122580233+SabeDoesThings@users.noreply.github.com>
2024-04-14 15:50:17 -0500

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -6,6 +6,11 @@ class Main extends hxd.App {
     }
 
     static function main() {
+        #if sys
+        hxd.Res.initLocal(); // important! allows the app access to our game's resource files: images (sprites), audio, etc.
+        #else
+        hxd.Res.initEmbed(); // use hxd.Res.initEmbed(); for html5/js
+        #end
         new Main();
     }
 }
