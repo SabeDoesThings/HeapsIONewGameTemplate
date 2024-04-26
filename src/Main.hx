b14c7c4dbdc6edae112a4e327e76f72e80ffd488
SabeDoesThings <122580233+SabeDoesThings@users.noreply.github.com>
2024-04-26 15:45:36 -0500

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -5,12 +5,20 @@ class Main extends hxd.App {
         tf.scale(4);
     }
 
+    public static var inst(get, null): Main;
+    static function get_inst(): Main {
+        if (inst == null)
+            inst = new Main();
+
+        return inst;
+    }
     static function main() {
         #if sys
         hxd.Res.initLocal(); // important! allows the app access to our game's resource files: images (sprites), audio, etc.
         #else
         hxd.Res.initEmbed(); // use hxd.Res.initEmbed(); for html5/js
         #end
-        new Main();
+
+        inst = new Main();
     }
 }
